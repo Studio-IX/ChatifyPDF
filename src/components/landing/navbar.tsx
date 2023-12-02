42510b0f932b3d70387cd3231245d284a46d20fb
Braimah-Abiola <braimahabiola.official@gmail.com>
2023-12-02 19:32:21 +0000

    Reverted trpc

---
+++ src/components/landing/navbar.tsx
@@ -0,0 +1,53 @@
+import Link from "next/link";
+import MaxWidthWrapper from "../MaxWidthWrapper";
+import { buttonVariants } from "../ui/button";
+import { LoginLink, RegisterLink } from "@kinde-oss/kinde-auth-nextjs/server";
+
+const Navbar = () => {
+  return (
+    <nav className="sticky h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
+      <MaxWidthWrapper>
+        <div className="flex h-14 items-center justify-between border-b border-zinc-200">
+          <Link href="/" className="flex z-40 font-semibold">
+            <h3 className="text-black">
+              <span className="text-primary">Chatify</span>PDF.
+            </h3>
+          </Link>
+
+          {/* todo: add mobile navbar */}
+
+          <div className="hidden items-center space-x-4 sm:flex">
+            <>
+              <Link
+                href="/pricing"
+                className={buttonVariants({
+                  variant: "ghost",
+                  size: "sm",
+                })}
+              >
+                Pricing
+              </Link>
+              <LoginLink
+                className={buttonVariants({
+                  variant: "ghost",
+                  size: "sm",
+                })}
+              >
+                Sign In
+              </LoginLink>
+              <RegisterLink
+                className={buttonVariants({
+                  size: "sm",
+                })}
+              >
+                Get Started
+              </RegisterLink>
+            </>
+          </div>
+        </div>
+      </MaxWidthWrapper>
+    </nav>
+  );
+};
+
+export default Navbar;
