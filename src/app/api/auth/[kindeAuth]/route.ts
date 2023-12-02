3a0f7cd6dec4b724d19decd41c28cd40166b0d06
Braimah-Abiola <braimahabiola.official@gmail.com>
2023-12-02 19:52:47 +0000

    Fixing build error

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -1,7 +1,21 @@
 import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(request: NextRequest, { params }: any) {
+export async function GET(
+  request: NextRequest,
+  { params }: any
+): Promise<void | NextResponse<unknown>> {
   const endpoint = params.kindeAuth;
-  return handleAuth(request, endpoint);
+
+  // Assuming handleAuth returns a function
+  const authHandler = handleAuth(request, endpoint);
+
+  // Call the returned function to get the response
+  const response = await authHandler(request, new NextResponse());
+
+  // Wrap the response using NextResponse if necessary
+  return new NextResponse(response.body, {
+    status: response.status,
+    headers: response.headers,
+  });
 }
