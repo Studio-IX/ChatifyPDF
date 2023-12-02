6467eda81681c29b668c2a55644cc3588cb55bb1
Braimah-Abiola <braimahabiola.official@gmail.com>
2023-12-02 23:21:22 +0000

    3hrs in

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,15 +1,21 @@
+import Dashboard from "@/components/dashboard/dashboard";
+import { db } from "@/db";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { redirect } from "next/navigation";
 
-export default async function Dashboard() {
+export default async function Page() {
   const { getUser } = getKindeServerSession();
   const user = await getUser();
 
-    if (!user || !user.id) redirect('/auth-callback?origin=dashboard');
+  if (!user || !user.id) redirect("/auth-callback?origin=dashboard");
 
-  return (
-    <div>
-        {user?.email}
-    </div>
-  );
+  const dbUser = await db.user.findFirst({
+    where: {
+      id: user.id,
+    },
+  });
+
+  if (!dbUser) redirect("/auth-callback?origin=dashboard");
+
+  return <Dashboard />;
 }
